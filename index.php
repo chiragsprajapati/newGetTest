2f3f348293b49e0722c196969bc86f468946393c
chiragsprajapati <chrgprajapati@gmail.com>
2019-03-14 22:26:34 +0530

    First test comment

---
+++ index.php
@@ -0,0 +1,103 @@
+<link href="//netdna.bootstrapcdn.com/bootstrap/3.0.3/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//netdna.bootstrapcdn.com/bootstrap/3.0.3/js/bootstrap.min.js"></script>
+<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+<!------ Include the above in your HEAD tag ---------->
+
+<h3 class="panel-title">
+                       Test git</h3>
+<div class="container">
+    <div class="row">
+        <div class="col-md-4">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <h3 class="panel-title">
+                        Panel Default</h3>
+                </div>
+                <ul class="list-group">
+                    <a href="#" class="list-group-item">Cras justo odio</a>
+                    <a href="http://www.jquery2dotnet.com/2014/01/two-way-scrolling-in-single-div.html" class="list-group-item active">Dapibus ac facilisis in</a>
+                    <a href="#" class="list-group-item">Morbi leo risus</a>
+                    <a href="#" class="list-group-item">Porta ac consectetur ac</a>
+                    <a href="#" class="list-group-item">Vestibulum at eros</a>
+                </ul>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="panel panel-primary">
+                <div class="panel-heading">
+                    <h3 class="panel-title">
+                        Panel Primary</h3>
+                </div>
+                <ul class="list-group">
+                    <a href="#" class="list-group-item">Cras justo odio</a>
+                    <a href="http://www.jquery2dotnet.com/2014/01/jquery-export-table-data-into-ms-excel.html" class="list-group-item active">Dapibus ac facilisis in</a>
+                    <a href="#" class="list-group-item">Morbi leo risus</a>
+                    <a href="#" class="list-group-item">Porta ac consectetur ac</a>
+                    <a href="#" class="list-group-item">Vestibulum at eros</a>
+                </ul>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="panel panel-success">
+                <div class="panel-heading">
+                    <h3 class="panel-title">
+                        Panel Success</h3>
+                </div>
+                <ul class="list-group">
+                    <a href="#" class="list-group-item">Cras justo odio</a>
+                    <a href="http://www.jquery2dotnet.com/2013/12/input-cursor-design-using-css3.html" class="list-group-item active">Dapibus ac facilisis in</a>
+                    <a href="#" class="list-group-item">Morbi leo risus</a>
+                    <a href="#" class="list-group-item">Porta ac consectetur ac</a>
+                    <a href="#" class="list-group-item">Vestibulum at eros</a>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-4">
+            <div class="panel panel-info">
+                <div class="panel-heading">
+                    <h3 class="panel-title">
+                        Panel Info</h3>
+                </div>
+                <ul class="list-group">
+                    <a href="#" class="list-group-item">Cras justo odio</a>
+                    <a href="http://www.jquery2dotnet.com/2013/12/cool-simple-css-drop-down-menu.html" class="list-group-item active">Dapibus ac facilisis in</a>
+                    <a href="#" class="list-group-item">Morbi leo risus</a>
+                    <a href="#" class="list-group-item">Porta ac consectetur ac</a>
+                    <a href="#" class="list-group-item">Vestibulum at eros</a>
+                </ul>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="panel panel-warning">
+                <div class="panel-heading">
+                    <h3 class="panel-title">
+                        Panel Warning</h3>
+                </div>
+                <ul class="list-group">
+                    <a href="#" class="list-group-item">Cras justo odio</a>
+                    <a href="http://www.jquery2dotnet.com/2013/11/bootstrap-navigation-custom-design.html" class="list-group-item active">Dapibus ac facilisis in</a>
+                    <a href="#" class="list-group-item">Morbi leo risus</a>
+                    <a href="#" class="list-group-item">Porta ac consectetur ac</a>
+                    <a href="#" class="list-group-item">Vestibulum at eros</a>
+                </ul>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="panel panel-danger">
+                <div class="panel-heading">
+                    <h3 class="panel-title">
+                        Panel Danger</h3>
+                </div>
+                <ul class="list-group">
+                    <a href="#" class="list-group-item">Cras justo odio</a>
+                    <a href="http://www.jquery2dotnet.com/2013/11/image-preview-on-hover-bootstrap.html" class="list-group-item active">Dapibus ac facilisis in</a>
+                    <a href="#" class="list-group-item">Morbi leo risus</a>
+                    <a href="#" class="list-group-item">Porta ac consectetur ac</a>
+                    <a href="#" class="list-group-item">Vestibulum at eros</a>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
